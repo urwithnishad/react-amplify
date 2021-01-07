695b36802bf4c94f007eb023fd1965b1fe149055
Nishad <urwithnishad@gmail.com>
2021-01-07 23:10:09 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Me
+          Learn React From Nishad
         </a>
       </header>
     </div>
