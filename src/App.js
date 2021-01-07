15e2fbd495ae5b3f4d1da946c6c5b9d9f22cbb17
Nishad <urwithnishad@gmail.com>
2021-01-07 23:18:15 +0530

    checking amp

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Nishad
+          Hello! We will Learn Amplify!
         </a>
       </header>
     </div>
